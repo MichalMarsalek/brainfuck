d6cf5d5d01b1130d003988c62d871084dcb13ef6
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-03-11 21:23:14 +0100

    rewrite compiler from scratch
    it now finds the best memory layout

--- asm/README.md
+++ asm/README.md
@@ -5,33 +5,25 @@ When this condition is met, the program uses a fixed amount of registers that yo
 Input to this tools is a list of commands each of which is transformed to some brainfuck characters and appended to the original line. This yields a readable version of a brainfuck code (for interpreters that ignore non-bf characters). Alternatively, you can choose to output just the brainfuck.
 
 ## Example
-Following program prints the letters "A"-"Z" 10 times on 10 separate lines.
+Following program prints the letters "A"-"Z"
 
 ```
-set NL 10       ++++++++++
-set rep 10      >++++++++++
-while rep       [
-  dec rep       -
-  clear letter  >[-]
-  set letter 65 >----[<+>----]<++
-  set rem 26    >>+++++[<+++++>-]<+
-  while rem     [
-    dec rem     -
-    out letter  <.
-    inc letter  +
-  end           >]
-  out NL        <<<.
-end             >]
+set rep letter 26
+set letter _ 64
+while rep
+    dec rep
+    inc letter
+    out letter
+end
 ```
 
 ## Commands
-* `def register` - doesn't produce any bf but defines a register
 * `clear register` - goes to the cell `register` and sets it to 0
-* `dec register` - goes to the cell `register` and decrements
-* `inc register` - goes to the cell `register` and increments
+* `dec register [amount=1]` - goes to the cell `register` and decrements it by the given amount
+* `inc register [amount=1]` - goes to the cell `register` and incrementsit by the given amount
 * `out register` - goes to the cell `register` and outputs
 * `in register` - goes to the cell `register` and gets input
-* `set register c` - goes to the cell `register` and sets it to the constant `c` assuming the cell and the one to the right of it is zero
+* `set register dummy c` - goes to the cell `register` and sets it to the constant `c` assuming both registers `register`and `dummy` are zero
 * `while register` - goes to the cell `register` and starts a loop
 * `end` - goes to the`register` cell of the matching `while` command and ends a loop
 * `repeat reg1 reg2` - stars a nondestructive loop of length `reg2`. Assumes `reg1` starts at zero and doesn't change inside the loop
@@ -46,7 +38,7 @@ while reg1
     inc reg2
 ```
 
-Registers are given numbers 0, 1, ... in the order they first appear in code. Note that you don't need to define registers with `def`, nondefined registers are auto defined using any command.
+All registers that appear anywhere in the code are given a position on the tape. The layout minimizing the brainfuck code length is chosen.
 
 All lines starting with a noncommand and all words after a command are ignored. This means, you can send the output of this tool to its input. Also you can add comments to the source easily.
 
--- asm/asm.nim
+++ asm/asm.nim
@@ -1,127 +1,124 @@
 include prelude
+import algorithm
 
-const constants = split " + ++ +++ ++++ +++++ ++++++ +++++++ ++++++++ +++++++++ ++++++++++ +++++++++++ ++++++++++++ +++++++++++++ ++++++++++++++ >+++[<+++++>-]< >++++[<++++>-]< >++++[<++++>-]<+ >+++[<++++++>-]< >+++[<++++++>-]<+ >++++[<+++++>-]< >+++[<+++++++>-]< >+++[<+++++++>-]<+ >++++[<++++++>-]<- >++++[<++++++>-]< >+++++[<+++++>-]< >+++++[<+++++>-]<+ >+++[<+++++++++>-]< >++++[<+++++++>-]< >++++[<+++++++>-]<+ >+++++[<++++++>-]< >+++++[<++++++>-]<+ >++++[<++++++++>-]< >++++[<++++++++>-]<+ >--[<-->+++++++]<-- >--[<-->+++++++]<- >--[<-->+++++++]< >---[<+>+++++++]< >---[<+>+++++++]<+ >---[<+>+++++++]<++ >--[<+>++++++]<--- >--[<+>++++++]<-- >--[<+>++++++]<- >--[<+>++++++]< >--[<+>++++++]<+ >--[<+>++++++]<++ >--[<+>++++++]<+++ >-[<+>-----]<---- >-[<+>-----]<--- >-[<+>-----]<-- >-[<+>-----]<- >-[<+>-----]< >-[<+>-----]<+ >-[<+>-----]<++ >-[<+>-----]<+++ >-[<+>-----]<++++ >-[<+>-----]<+++++ >-[<+>+++++++++]< >-[<+>+++++++++]<+ >----[<+>----]<---- >----[<+>----]<--- >----[<+>----]<-- >----[<+>----]<- >----[<+>----]< >----[<+>----]<+ >----[<+>----]<++ >----[<+>----]<+++ >----[<--->----]< >----[<--->----]<+ >----[<--->----]<++ >-[<+>-------]<--- >-[<+>-------]<-- >-[<+>-------]<- >-[<+>-------]< >-[<+>-------]<+ >-[<+>-------]<++ >-[<+>-------]<+++ >-[<+>---]<-------- >-[<+>---]<------- >-[<+>---]<------ >-[<+>---]<----- >-[<+>---]<---- >-[<+>---]<--- >-[<+>---]<-- >-[<+>---]<- >-[<+>---]< >-[<+>---]<+ >-[<+>---]<++ >-[<+>---]<+++ >-[<+>---]<++++ >-[<+>---]<+++++ >-[<+>---]<++++++ >-[<+>---]<+++++++ >-[<+>---]<++++++++ >-[<+>---]<+++++++++ >-[<++>-----]<------- >-[<++>-----]<------ >-[<++>-----]<----- >-[<++>-----]<---- >-[<++>-----]<--- >-[<++>-----]<-- >-[<++>-----]<- >-[<++>-----]< >-[<++>-----]<+ >-[<++>-----]<++ >-[<++>-----]<+++ >-[<++>-----]<++++ >-[<++>-----]<+++++ >-[<-->-------]<-- >-[<-->-------]<- >-[<-->-------]< >-[<-->-------]<+ >-[<-->-------]<++ >-[<++>+++++++++]<- >-[<++>+++++++++]< >-[<++>+++++++++]<+ >--------[<+++>--]< >----[<+++++>--]<- >----[<+++++>--]< >------[<+++>--]< >----[<+++>--]<-- >----[<+++>--]<- >----[<+++>--]< >--[<+>--]<---- >--[<+>--]<--- >--[<+>--]<-- >--[<+>--]<- >--[<+>--]< >--[<->--]<- >--[<->--]< >--[<->--]<+ >--[<->--]<++ >--[<->--]<+++ >--[<->--]<++++ >----[<--->--]< >----[<--->--]<+ >----[<--->--]<++ >------[<--->--]< >----[<----->--]< >----[<----->--]<+ >--------[<--->--]< >-[<-->+++++++++]<- >-[<-->+++++++++]< >-[<-->+++++++++]<+ >-[<++>-------]<-- >-[<++>-------]<- >-[<++>-------]< >-[<++>-------]<+ >-[<++>-------]<++ >-[<-->-----]<----- >-[<-->-----]<---- >-[<-->-----]<--- >-[<-->-----]<-- >-[<-->-----]<- >-[<-->-----]< >-[<-->-----]<+ >-[<-->-----]<++ >-[<-->-----]<+++ >-[<-->-----]<++++ >-[<-->-----]<+++++ >-[<-->-----]<++++++ >-[<-->-----]<+++++++ >-[<->---]<--------- >-[<->---]<-------- >-[<->---]<------- >-[<->---]<------ >-[<->---]<----- >-[<->---]<---- >-[<->---]<--- >-[<->---]<-- >-[<->---]<- >-[<->---]< >-[<->---]<+ >-[<->---]<++ >-[<->---]<+++ >-[<->---]<++++ >-[<->---]<+++++ >-[<->---]<++++++ >-[<->---]<+++++++ >-[<->---]<++++++++ >-[<->-------]<--- >-[<->-------]<-- >-[<->-------]<- >-[<->-------]< >-[<->-------]<+ >-[<->-------]<++ >-[<->-------]<+++ >----[<+++>----]<-- >----[<+++>----]<- >----[<+++>----]< >----[<->----]<--- >----[<->----]<-- >----[<->----]<- >----[<->----]< >----[<->----]<+ >----[<->----]<++ >----[<->----]<+++ >----[<->----]<++++ >-[<->+++++++++]<- >-[<->+++++++++]< >-[<->-----]<----- >-[<->-----]<---- >-[<->-----]<--- >-[<->-----]<-- >-[<->-----]<- >-[<->-----]< >-[<->-----]<+ >-[<->-----]<++ >-[<->-----]<+++ >-[<->-----]<++++ >--[<->++++++]<--- >--[<->++++++]<-- >--[<->++++++]<- >--[<->++++++]< >--[<->++++++]<+ >--[<->++++++]<++ >--[<->++++++]<+++ >---[<->+++++++]<-- >---[<->+++++++]<- >---[<->+++++++]< >--[<++>+++++++]< >--[<++>+++++++]<+ >--[<++>+++++++]<++ >----[<-------->+]<- >----[<-------->+]< >-----[<------>+]<- >-----[<------>+]< >----[<------->+]<- >----[<------->+]< >---[<--------->+]< >-----[<----->+]<- >-----[<----->+]< >----[<------>+]< >----[<------>+]<+ >---[<------->+]<- >---[<------->+]< >----[<----->+]< >---[<------>+]<- >---[<------>+]< >----[<---->+]<- >----[<---->+]< >---[<----->+]< -------------- ------------- ------------ ----------- ---------- --------- -------- ------- ------ ----- ---- --- -- -"
+const constants = split " + ++ +++ ++++ +++++ ++++++ +++++++ ++++++++ +++++++++ ++++++++++ +++++++++++ ++++++++++++ +++++++++++++ ++++++++++++++ +++[A+++++B-]A ++++[A++++B-]A ++++[A++++B-]A+ +++[A++++++B-]A +++[A++++++B-]A+ ++++[A+++++B-]A +++[A+++++++B-]A +++[A+++++++B-]A+ ++++[A++++++B-]A- ++++[A++++++B-]A +++++[A+++++B-]A +++++[A+++++B-]A+ +++[A+++++++++B-]A ++++[A+++++++B-]A ++++[A+++++++B-]A+ +++++[A++++++B-]A +++++[A++++++B-]A+ ++++[A++++++++B-]A ++++[A++++++++B-]A+ --[A--B+++++++]A-- --[A--B+++++++]A- --[A--B+++++++]A ---[A+B+++++++]A ---[A+B+++++++]A+ ---[A+B+++++++]A++ --[A+B++++++]A--- --[A+B++++++]A-- --[A+B++++++]A- --[A+B++++++]A --[A+B++++++]A+ --[A+B++++++]A++ --[A+B++++++]A+++ -[A+B-----]A---- -[A+B-----]A--- -[A+B-----]A-- -[A+B-----]A- -[A+B-----]A -[A+B-----]A+ -[A+B-----]A++ -[A+B-----]A+++ -[A+B-----]A++++ -[A+B-----]A+++++ -[A+B+++++++++]A -[A+B+++++++++]A+ ----[A+B----]A---- ----[A+B----]A--- ----[A+B----]A-- ----[A+B----]A- ----[A+B----]A ----[A+B----]A+ ----[A+B----]A++ ----[A+B----]A+++ ----[A---B----]A ----[A---B----]A+ ----[A---B----]A++ -[A+B-------]A--- -[A+B-------]A-- -[A+B-------]A- -[A+B-------]A -[A+B-------]A+ -[A+B-------]A++ -[A+B-------]A+++ -[A+B---]A-------- -[A+B---]A------- -[A+B---]A------ -[A+B---]A----- -[A+B---]A---- -[A+B---]A--- -[A+B---]A-- -[A+B---]A- -[A+B---]A -[A+B---]A+ -[A+B---]A++ -[A+B---]A+++ -[A+B---]A++++ -[A+B---]A+++++ -[A+B---]A++++++ -[A+B---]A+++++++ -[A+B---]A++++++++ -[A+B---]A+++++++++ -[A++B-----]A------- -[A++B-----]A------ -[A++B-----]A----- -[A++B-----]A---- -[A++B-----]A--- -[A++B-----]A-- -[A++B-----]A- -[A++B-----]A -[A++B-----]A+ -[A++B-----]A++ -[A++B-----]A+++ -[A++B-----]A++++ -[A++B-----]A+++++ -[A--B-------]A-- -[A--B-------]A- -[A--B-------]A -[A--B-------]A+ -[A--B-------]A++ -[A++B+++++++++]A- -[A++B+++++++++]A -[A++B+++++++++]A+ --------[A+++B--]A ----[A+++++B--]A- ----[A+++++B--]A ------[A+++B--]A ----[A+++B--]A-- ----[A+++B--]A- ----[A+++B--]A --[A+B--]A---- --[A+B--]A--- --[A+B--]A-- --[A+B--]A- --[A+B--]A --[A-B--]A- --[A-B--]A --[A-B--]A+ --[A-B--]A++ --[A-B--]A+++ --[A-B--]A++++ ----[A---B--]A ----[A---B--]A+ ----[A---B--]A++ ------[A---B--]A ----[A-----B--]A ----[A-----B--]A+ --------[A---B--]A -[A--B+++++++++]A- -[A--B+++++++++]A -[A--B+++++++++]A+ -[A++B-------]A-- -[A++B-------]A- -[A++B-------]A -[A++B-------]A+ -[A++B-------]A++ -[A--B-----]A----- -[A--B-----]A---- -[A--B-----]A--- -[A--B-----]A-- -[A--B-----]A- -[A--B-----]A -[A--B-----]A+ -[A--B-----]A++ -[A--B-----]A+++ -[A--B-----]A++++ -[A--B-----]A+++++ -[A--B-----]A++++++ -[A--B-----]A+++++++ -[A-B---]A--------- -[A-B---]A-------- -[A-B---]A------- -[A-B---]A------ -[A-B---]A----- -[A-B---]A---- -[A-B---]A--- -[A-B---]A-- -[A-B---]A- -[A-B---]A -[A-B---]A+ -[A-B---]A++ -[A-B---]A+++ -[A-B---]A++++ -[A-B---]A+++++ -[A-B---]A++++++ -[A-B---]A+++++++ -[A-B---]A++++++++ -[A-B-------]A--- -[A-B-------]A-- -[A-B-------]A- -[A-B-------]A -[A-B-------]A+ -[A-B-------]A++ -[A-B-------]A+++ ----[A+++B----]A-- ----[A+++B----]A- ----[A+++B----]A ----[A-B----]A--- ----[A-B----]A-- ----[A-B----]A- ----[A-B----]A ----[A-B----]A+ ----[A-B----]A++ ----[A-B----]A+++ ----[A-B----]A++++ -[A-B+++++++++]A- -[A-B+++++++++]A -[A-B-----]A----- -[A-B-----]A---- -[A-B-----]A--- -[A-B-----]A-- -[A-B-----]A- -[A-B-----]A -[A-B-----]A+ -[A-B-----]A++ -[A-B-----]A+++ -[A-B-----]A++++ --[A-B++++++]A--- --[A-B++++++]A-- --[A-B++++++]A- --[A-B++++++]A --[A-B++++++]A+ --[A-B++++++]A++ --[A-B++++++]A+++ ---[A-B+++++++]A-- ---[A-B+++++++]A- ---[A-B+++++++]A --[A++B+++++++]A --[A++B+++++++]A+ --[A++B+++++++]A++ ----[A--------B+]A- ----[A--------B+]A -----[A------B+]A- -----[A------B+]A ----[A-------B+]A- ----[A-------B+]A ---[A---------B+]A -----[A-----B+]A- -----[A-----B+]A ----[A------B+]A ----[A------B+]A+ ---[A-------B+]A- ---[A-------B+]A ----[A-----B+]A ---[A------B+]A- ---[A------B+]A ----[A----B+]A- ----[A----B+]A ---[A-----B+]A -------------- ------------- ------------ ----------- ---------- --------- -------- ------- ------ ----- ---- --- -- -"
 
-proc getBfLines(lines: seq[string]): seq[string] =
+type Command = enum Inc, Dec, Clear, Out, In, Set, While, End, Repeat, Comment
+type Instruction = object
+  kind: Command
+  reg1, reg2, amount: int
+type Program = object
+  instructions: seq[Instruction]
+  regCount: int
+  
+func toByteCode(source: seq[string]): (Program, seq[string]) =
   var registers: Table[string, int]
-  var point = 0
-  var stack: seq[string]
-  proc def(register: string) =
+  var registersSeq: seq[string]
+  var regCount = 0
+  proc getReg(register: string): int =
     if not registers.hasKey(register):
-      registers[register] = registers.len    
-  proc goto(register: string): string =
-    def(register)
-    while point < registers[register]:
-      result &= ">"
-      inc point
-    while point > registers[register]:
-      result &= "<"
-      dec point
-    
-  for line in lines:
+      registers[register] = registers.len
+      inc regCount
+      registersSeq &= register
+    return registers[register]
+  var stack: seq[int]
+  for line in source:
     let w = line.strip.split
-    result &= (case w[0]
-    of "def":   (def(w[1]);"")
-    of "clear": goto(w[1]) & "[-]"
-    of "dec":   goto(w[1]) & "-"
-    of "inc":   goto(w[1]) & "+"
-    of "out":   goto(w[1]) & "."
-    of "in":    goto(w[1]) & ","
-    of "set":   replace(goto(w[1]) & constants[w[2].parseInt], "<>", "")
-    of "while": (stack &= w[1]; goto(w[1]) & "[")
-    of "end":   goto(stack.pop) & "]"
-    of "repeat": (stack &= w[1]; goto(w[2]) & "[-" & goto(w[1]) & "+" & goto(w[2]) & "]" & goto(w[1]) & "[-" & goto(w[2]) & "+")
-    else:       "")
+    result[0].instructions &= (case w[0]
+    of "clear": Instruction(kind: Command.Clear, reg1: getReg(w[1]))
+    of "dec":   Instruction(kind: Command.Dec, reg1: getReg(w[1]), amount: (try:w[2].parseInt except: 1))
+    of "inc":   Instruction(kind: Command.Inc, reg1: getReg(w[1]), amount: (try:w[2].parseInt except: 1))
+    of "out":   Instruction(kind: Command.Out, reg1: getReg(w[1]))
+    of "in":    Instruction(kind: Command.In, reg1: getReg(w[1]))
+    of "set":   Instruction(kind: Command.Set, reg1: getReg(w[1]), reg2: getReg(w[2]), amount: w[3].parseInt)
+    of "while": (stack &= getReg(w[1]); Instruction(kind: Command.While, reg1: getReg(w[1])))
+    of "end":   Instruction(kind: Command.End, reg1: stack.pop)
+    of "repeat":(stack &= getReg(w[1]); Instruction(kind: Command.Repeat, reg1: getReg(w[1]), reg2: getReg(w[2])))
+    else:       Instruction(kind: Command.Comment))
+  result[0].regCount = regCount
+  result[1] = registersSeq
 
-proc compile(source:string, golf=false): string =
+func getLength(program: var Program, perm: var seq[int]): int =
+    var point = 0
+    var freeMove = true
+    template goto(reg: int) =
+        let reg2 = reg
+        if freeMove:
+            freeMove = false
+        else:
+            result += abs(perm[reg2] - point)
+        point = perm[reg2]
+    for ins in program.instructions:
+        case ins.kind
+        of Command.Comment: discard
+        of Command.Set: goto(ins.reg2); goto(ins.reg1); goto(ins.reg2); goto(ins.reg1)
+        of Command.Repeat: goto(ins.reg2); goto(ins.reg1);goto(ins.reg2); goto(ins.reg1);goto(ins.reg2)
+        else: goto(ins.reg1)
+
+func getBrainfuck(program: var Program, perm: var seq[int]): seq[string] =
+    var point = 0
+    var freeMove = true
+    template goto(reg: int): string =
+        let reg2 = reg        
+        var temp = ""
+        if freeMove:
+            freeMove = false
+        else:
+            temp = repeat(if perm[reg2] > point: ">" else: "<", abs(perm[reg2] - point))
+        point = perm[reg2]
+        temp
+    for ins in program.instructions:
+        result &= (case ins.kind
+        of Command.Comment: ""
+        of Command.End: goto(ins.reg1) & "]"
+        of Command.Set: goto(ins.reg2) & constants[ins.amount].replace("A", goto(ins.reg1)).replace("B", goto(ins.reg2)).replace("A", goto(ins.reg1))
+        of Command.While: goto(ins.reg1) & "["
+        of Command.Repeat: goto(ins.reg2) & "[-" & goto(ins.reg1) & "+" & goto(ins.reg2) & "]" & goto(ins.reg1) & "[-" & goto(ins.reg2) & "+"
+        of Command.Clear: goto(ins.reg1) & "[-]"
+        of Command.Dec: goto(ins.reg1) & "-".repeat(ins.amount)
+        of Command.Inc: goto(ins.reg1) & "+".repeat(ins.amount)
+        of Command.Out: goto(ins.reg1) & "."
+        of Command.In: goto(ins.reg1) & ","
+        )
+
+func interpret(program: Program): string =
+    if program.instructions.anyIt(it.kind == Command.In): return
+    return ""
+
+func getOptimalBrainfuck(program: var Program): (seq[string], seq[int]) =
+    var perm = toSeq(0..<program.regCount)
+    var minLength = int.high
+    var minPerm: seq[int]
+    while true:
+        let length = getLength(program, perm)
+        if length < minLength:
+            minLength = length
+            minPerm = perm
+        if not perm.nextPermutation: break
+    return (getBrainfuck(program, minPerm), minPerm)
+
+proc compile(source:string, golf=false) =
   var lines = source.strip.splitLines
-  let bf = lines.getBfLines
-  if golf: return bf.join("")
+  var (program, registers) = toByteCode(lines)
+  let (bfLines, perm) = program.getOptimalBrainfuck
+  registers = perm.mapIt(registers[it])
+  let bf = bfLines.join
+  echo "Length: ", $bf.len
+  echo "Memory layout: ", registers.join(" ")
+  echo "               ", toSeq(0..<registers.len).mapIt(($it).align(registers[it].len)).join(" ")
+  echo "Golfed: ", bf
   let maxlen = lines.mapIt(it.len).max
   for i in 0..<lines.len:
-    lines[i] = lines[i].alignLeft(maxlen + 1) & bf[i]
-  return lines.join("\n")
-
-var code0 = """
-set size 7
-set i 10
-def j
-while i
-    dec i
-    set space 3
-    set star 4
-    inc NL
-end
-set space 2
-set star 2
-repeat i size
-    inc size
-    inc size
-    clear stars
-    inc stars
-    repeat j size
-        repeat k j
-            out space
-        end
-        out space
-        repeat k stars
-            out star
-        end
-        inc stars
-        inc stars
-        out NL
-    end
-    repeat j size
-        out space
-    end
-    out star
-    out NL
-    out NL
-    dec size
-    dec size
-end
-"""
+    echo lines[i].alignLeft(maxlen + 1), bfLines[i]
 
 var code = """
-set size 7
-set i 10
-def j
-while i
-    dec i
-    set space 3
-    set star 4
-    inc NL
-end
-set space 2
-set star 2
-repeat i size
-    inc size
-    inc size
-    clear stars
-    inc stars
-    repeat j size
-        repeat k j
-            out space
-        end
-        out space
-        repeat k stars
-            out star
-        end
-        inc stars
-        inc stars
-        out NL
-    end
-    repeat j size
-        out space
-    end
-    out star
-    out NL
-    out NL
-    dec size
-    dec size
+set rep letter 26
+set letter _ 64
+while rep
+    dec rep
+    inc letter
+    out letter
 end
 """
 
-echo code.compile(true).len
-echo code.compile(true)
-echo code.compile
+compile code
